255a405f9437bec841ff22f5f54f48b772dc089d
Michał Szostak <szostak.m.f@gmail.com>
2016-12-21 14:15:55 +0100

    included index.html in docs generation + .htaccess with index

--- README.md
+++ README.md
@@ -36,3 +36,13 @@ See docs: <!--[http://link_to_docs/developer]--> (TODO)
 Testing requires running agreemount.engine backend with the appropriate
 version that tested angular-engine version supports. Everything needed
 to start is located in `/tests/e2e`
+
+##Documentation
+
+To build documentation execute (from the top repository level):
+``` 
+docs/build/build.sh
+```
+Result of the script is documentation for all angular-engine versions
+created in `buid/` directory in respective dirs mirroring versions of
+angular-engine package
--- docs/build/build.sh
+++ docs/build/build.sh
@@ -5,28 +5,36 @@ export PATH=node_modules/.bin:$PATH
 npm install
 
 first_documented_version='v0.6.12'
-start_documenting=0
+newest_version=1
+stop_documenting=0
 
 npm install
-
-for git_tag in `git tag --sort=v:refname`
+for git_tag in `git tag --sort=-v:refname`
 do
-    if [ $start_documenting -eq 0 ]; then
-        if [ $git_tag != $first_documented_version ];
-        then
-            continue;
-        else
-        	grunt clean
-            start_documenting=1;
-    	fi;
+    if [ $stop_documenting -eq 0 ]; then
+        if [ $git_tag == $first_documented_version ]; then
+            stop_documenting=1
+        fi;
+
+        if [ $newest_version -eq 1 ]; then
+            grunt clean
+            mkdir build
+
+            git_version=${git_tag:1}
+            sed  "s/@@__ENGINE_VERSION__/$git_version/g" docs/build/index.tpl.html > build/index.html
+            sed  "s/@@__ENGINE_VERSION__/$git_version/g" docs/build/.htaccess > build/.htaccess
+            newest_version=0
+        fi;
+        git checkout angular-engine.js
+        git checkout $git_tag
+        if [ $? -ne 0 ]; then
+            echo "Could not change branch, exiting."
+            exit 1;
+        fi;
+        brunch build
+    else
+        break;
     fi;
-    git checkout angular-engine.js
-	git checkout $git_tag
-	if [ $? -ne 0 ]; then
-        echo "Could not change branch, exiting."
-        exit 1;
-	fi;
-	brunch build
 done
 
 # revert to master
