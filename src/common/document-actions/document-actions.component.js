3951d329cdb8260774a0d65a817307fc1cc10444
Krzysztof Marszałek <marszalekk@gmail.com>
2017-06-05 15:59:07 +0200

    added display condition for validate action

--- src/common/document-actions/document-actions.component.js
+++ src/common/document-actions/document-actions.component.js
@@ -45,6 +45,7 @@ angular.module('engine.common')
         options: '=',
         steps: '=',
         step: '=',
+        actions: '=',
         showValidationButton: '=',
         customButtons: '=',
         documentParent: '='
