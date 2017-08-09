b737088edf1516144bec46fdb10bd74cd096dd57
Jakub Sawicki <ymsawick@cyfronet.pl>
2017-08-09 17:00:25 +0200

    Allow to constrain uploaded file by extension and media type

--- src/document/services/document.form.attachments.js
+++ src/document/services/document.form.attachments.js
@@ -192,6 +192,7 @@ angular.module('engine.document').controller('engAttachmentCtrl', function ($sco
         $scope.error = null;
         $scope.STATUS = STATUS;
         $scope.status = STATUS.loading;
+        $scope.accept = $scope.metric.accept || '';
         if ($scope.ctx.document.id != null) {
             $scope.attachment = new engAttachment($scope.ctx.document, $scope.metric.id, $scope.isList);
             $scope.attachment.ready.then(function () {
