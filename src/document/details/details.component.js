7c371eca462b7604e5539be85d42e7b255b54cb4
Marta Świątkowska <martaswiatkowska8@gmail.com>
2017-10-20 12:04:49 +0200

    Added directive scroll-rwd to sidebar-document-rwd element

--- src/document/details/details.component.js
+++ src/document/details/details.component.js
@@ -1,10 +1,12 @@
 angular.module('engine.document')
     .component('engineDocumentDetails', {
         templateUrl: '/src/document/details/details.tpl.html',
-        controller: function ($parse) {
+        controller: function ($parse, $window, $scope) {
             var self = this;
             this.$parse = $parse;
-
+            self.isVisible = function () {
+                return $scope.$parent.sideMenuVisible;
+            }
             this.saveDocument = function () {
                 self.savePromise = self.actions.callSave();
                 return self.savePromise;
@@ -17,14 +19,53 @@ angular.module('engine.document')
             dirty: '='
         }
     })
-.filter('conditionFulfiled', function ($parse) {
-   return function (items, document) {
-       var filtered = [];
+    .filter('conditionFulfiled', function ($parse) {
+        return function (items, document) {
+            var filtered = [];
 
-       angular.forEach(items, function (item) {
-           if(item.condition == null || $parse(item.condition)(document) === true)
-               filtered.push(item);
-       });
-       return filtered;
-   }
-});
+            angular.forEach(items, function (item) {
+                if (item.condition == null || $parse(item.condition)(document) === true)
+                    filtered.push(item);
+            });
+            return filtered;
+        }
+    })
+    .directive('scrollRwd', ['$window', '$document', '$timeout', function ($window, $document, $timeout) {
+        return {
+            restrict: 'AEC',
+            link: link
+        };
+        function link(scope, element, attrs) {
+            scope.windowHeight = $window.innerHeight;
+
+            attrs.$observe('isVisible', function (val) {
+                if (val) {
+                    $timeout(function () {
+                        scope.windowHeight = $window.innerHeight;
+                        updateScroll()
+                    });
+                }
+            });
+
+            angular.element($window).on('resize', function () {
+                scope.windowHeight = $window.innerHeight;
+                updateScroll();
+                scope.$digest();
+            });
+
+            function updateScroll() {
+                var marginBottom = 10;
+                var elementHeight = element[0].scrollHeight;
+                var elementTop = element[0].getBoundingClientRect().top;
+
+                if (elementHeight > 0 && elementHeight + elementTop > scope.windowHeight) {
+                    var heightToSave = scope.windowHeight - elementTop - marginBottom + "px";
+                    element.css("height", heightToSave);
+                    element.addClass("scroll-rwd")
+                } else {
+                    element.css("height", "auto");
+                    element.removeClass("scroll-rwd")
+                }
+            }
+        }
+    }]);
