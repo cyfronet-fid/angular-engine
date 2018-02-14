e228cfe5cfa1679a0bbeb39500296b0bc0da628d
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-02-14 13:55:38 +0100

    Added filter numberFormat to details

--- src/document/details/details.component.js
+++ src/document/details/details.component.js
@@ -79,4 +79,13 @@ angular.module('engine.document')
                 }
             }
         }
-    }]);
+    }]).filter('numberFormat', function ($translate) {
+        return function (item) {
+            var number = Number(item)
+            if(!_.isNaN(number) ){
+                return ( number.toLocaleString($translate.use()))
+            }else{
+                return (item)
+            }
+        }
+})
