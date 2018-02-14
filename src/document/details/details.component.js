255e22d037a161116f196341fda826ed4503602b
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-02-14 14:05:48 +0100

    added filter to text cells

--- src/document/details/details.component.js
+++ src/document/details/details.component.js
@@ -79,13 +79,4 @@ angular.module('engine.document')
                 }
             }
         }
-    }]).filter('numberFormat', function ($translate) {
-        return function (item) {
-            var number = Number(item)
-            if(!_.isNaN(number) ){
-                return ( number.toLocaleString($translate.use()))
-            }else{
-                return (item)
-            }
-        }
-})
+    }])
---
+++ src/document/numberFormatFilter.js
@@ -0,0 +1,13 @@
+/**
+ * Created by marta on 2/14/18.
+ */
+angular.module('engine.document').filter('numberFormat', function ($translate) {
+    return function (item) {
+        var number = Number(item)
+        if(!_.isNaN(number) ){
+            return ( number.toLocaleString($translate.use()))
+        }else{
+            return (item)
+        }
+    }
+})
